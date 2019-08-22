8c7845faf61136ed5b36f5d8538a46deccaa872e
jjzcru <jjzcru@gmail.com>
2019-08-22 16:49:46 -0500

    Move uuid from a function

--- Canvasflow.jsx
+++ Canvasflow.jsx
@@ -515,7 +515,13 @@ var CanvasflowBuild = function(settingsPath, commandFilePath) {
     $.getDocumentID = function(doc) {
         var uuid = $.getUUIDFromDocument(doc);
         if(!uuid) {
-            uuid = $.getUUID();
+            var dt = new Date().getTime();
+            var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+                var r = (dt + Math.random()*16)%16 | 0;
+                dt = Math.floor(dt/16);
+                return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+            });
+            uuid = uuid.substring(0, uuid.length / 2);
             doc.insertLabel("CANVASFLOW-ID", uuid);
         }
 
@@ -912,7 +918,7 @@ var CanvasflowBuild = function(settingsPath, commandFilePath) {
         originalImageFile.copy(imageDirectory + '/' + id + '.' + ext);
 
         if(originalImageSize >= $.imageSizeCap) {
-            $.imagesToResize.push(File(destFilePath).fsName);
+            $.imagesToResize.push(File(imageDirectory + '/' + id + '.' + ext).fsName);
             return '' + id + '.' + targetExt;
         }
         
