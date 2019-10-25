a4d6a100bf00d610cf1d1a1a0f7a0f5181f246b2
Jose J. Cruz Torres <jjzcru@gmail.com>
2019-10-25 10:04:34 -0500

    Save indesign settings with json stringnify

--- modules/CanvasflowSettings.js
+++ modules/CanvasflowSettings.js
@@ -1,7 +1,18 @@
+//@include "json2.js"
+
 var CanvasflowSettings = function(settingsPath){
     var $ = this;
     $.settingsPath = settingsPath;
-    $.defaultSavedSettings = '{"apiKey":"", "PublicationID": "", "IssueID": "", "StyleID": "", "endpoint": "", "pages": "", "creationMode": "document", "contentOrder": "natural"}';
+    $.defaultSavedSettings = JSON.stringify({
+        'apiKey': '',
+        'PublicationID': '',
+        'IssueID': '',
+        'StyleID': '',
+        'endpoint': '',
+        'pages': '',
+        'creationMode': 'document',
+        'contentOrder': 'natural'
+    });
 
     $.getSavedSettings = function() {
         var file = new File($.settingsPath);
@@ -23,15 +34,16 @@ var CanvasflowSettings = function(settingsPath){
         var file = new File($.settingsPath);
         file.encoding = 'UTF-8';
         file.open('w');
-        var content = '{"apiKey":"' + settings.apiKey + '"' +
-        ', "PublicationID": "' + settings.PublicationID + '"' +
-        ', "IssueID":"' + settings.IssueID + '"' + 
-        ', "StyleID": "' + settings.StyleID + '"' + 
-        ', "endpoint": "' + settings.endpoint + '"' +
-        ', "pages": "' + (settings.pages || '') + '"' + 
-        ', "creationMode": "' + (settings.creationMode || 'document') + '"' +
-        ', "contentOrder": "' + (settings.contentOrder || 'natural') + '"' +
-        '}';
+        var content = JSON.stringify({
+            'apiKey': settings.apiKey,
+            'PublicationID': settings.PublicationID,
+            'IssueID': settings.IssueID,
+            'StyleID': settings.StyleID,
+            'endpoint': settings.endpoint,
+            'pages': (settings.pages || ''),
+            'creationMode': (settings.creationMode || 'document'),
+            'contentOrder': (settings.contentOrder || 'natural')
+        });
         file.write(content);
         file.close();
     }
